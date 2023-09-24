9029b21652706485339fd7c3447d218e7a1224a8
voltara13 <stalker.nik20@gmail.com>
2023-09-24 23:02:33 +0700

    Added 1.1 lab

---
+++ clojure_1/src/clojure_1_2.clj
@@ -0,0 +1,25 @@
+(ns clojure_1_2)
+
+(defn get-all-str
+  "Get a list of all strings of length n,
+   consisting of characters from the alphabet list
+   and not containing two identical characters in a row."
+  [n alphabet]
+  (letfn [(add-to-result [current-string]
+    (if (= (count current-string) n)
+      (recur [current-string])
+      (letfn [(get-str-variant [letters]
+                (when (not-empty letters)
+                  (let [letter (first letters)]
+                    (if (or (= (count current-string) 0)
+                            (not= (last current-string) letter))
+                      (concat
+                       (add-to-result (str current-string letter))
+                       (get-str-variant (rest letters)))
+                      (get-str-variant (rest letters))))))]
+        (get-str-variant alphabet))))]
+    (cond
+      (<= n 0) []
+      (empty? alphabet) []
+      :else
+      (add-to-result ""))))
