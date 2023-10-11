aac1bb58fb54b1695a87b3c9285303de494017db
voltara13 <stalker.nik20@gmail.com>
2023-10-11 18:54:53 +0700

    Improvements for 2_1 lab

--- clojure_2/src/clojure_2_1.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure_2_1
-  (:require [clojure_2_0 :as src]))
-
-(def calc-integral-mem (memoize src/calc-integral))
--- clojure_2/test/clojure_2_1_test.clj
+++ clojure_2/test/clojure_2_1_test.clj
@@ -1,19 +1,25 @@
 (ns clojure_2_1_test
   (:require [clojure.test :refer [deftest is run-tests]]
-            [clojure_2_0 :as old]
-            [clojure_2_1 :as mem]
+            [clojure_2_0 :as src]
             [clojure_2_0_test :refer [close-to polinom indict linear power quad]]))
 
+(def linear-mem (memoize linear))
+(def quad-mem (memoize quad))
+(def power-mem (memoize power))
+(def indict-mem (memoize indict))
+(def polinom-mem (memoize polinom))
+
 (deftest test-calc-integral
-  (is (close-to 50 ((mem/calc-integral-mem linear) 10) 1e-3))
-  (is (close-to 333.333 ((mem/calc-integral-mem quad) 10) 1e-3))
-  (is (close-to 2500 ((mem/calc-integral-mem power) 10) 1e-3))
-  (is (close-to 12206981.203 ((mem/calc-integral-mem indict) 10) 1e-3))
-  (is (close-to 308033.238 ((mem/calc-integral-mem polinom) 100) 1e-3)))
+  (is (close-to 50 ((src/calc-integral linear-mem) 10) 1e-3))
+  (is (close-to 333.333 ((src/calc-integral quad-mem) 10) 1e-3))
+  (is (close-to 2500 ((src/calc-integral power-mem) 10) 1e-3))
+  (is (close-to 12206981.203 ((src/calc-integral indict-mem) 10) 1e-3))
+  (is (close-to 308033.238 ((src/calc-integral polinom-mem) 100) 1e-3)))
 
 (defn -main []
   (run-tests 'clojure_2_1_test)
-  (println "OLD:" (time ((old/calc-integral polinom) 100)))
-  (println "MEM FIRST:" (time ((mem/calc-integral-mem polinom) 100))))
+  (println "OLD:" (time ((src/calc-integral polinom-mem) 100)))
+  (println "MEM FIRST:" (time ((src/calc-integral polinom-mem) 100)))
+  (println "MEM SECOND:" (time ((src/calc-integral polinom-mem) 100))))
 
 (-main)
